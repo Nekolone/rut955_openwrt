1b1ff18e18a15af17751bb433dafbdf06bac147d
Nikolai Kottcov <nikolafgh@gmail.com>
2022-02-22 17:44:18 +0200

    first tcp client

--- cmd/main_app/main.go
+++ cmd/main_app/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("RutOS test1")
+	strEcho := "hi python server"
+	servAddr := "192.168.100.107:11113"
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(strEcho))
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("write to server = ", strEcho)
+
+	reply := make([]byte, 1024)
+
+	_, err = conn.Read(reply)
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("reply from server=", string(reply))
+
+	err = conn.Close()
+	if err != nil {
+		log.Fatal("Close error")
+		return
+	}
+
 }
