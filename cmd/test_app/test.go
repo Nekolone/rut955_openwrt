ae13ec63107811f0a6ab97a4f8060b33ed5d0806
Nikolai Kottcov <nikolafgh@gmail.com>
2022-03-09 18:49:57 +0200

    test modbus

--- cmd/test_app/test.go
+++ cmd/test_app/test.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	modbusclient "rut955_openwrt/pkg/go-modbus"
-	"strconv"
-	"strings"
 )
 
 func main() {
@@ -33,11 +29,24 @@ func main() {
 		os.Exit(1)
 	}
 
+	for i := 0x00; i < 0xff; i++ {
+		readData := make([]byte, 3)
+		readData[0] = byte(200 >> 8)   // (High Byte)
+		readData[1] = byte(200 & 0xff) // (Low Byte)
+		readData[2] = byte(i)
+
+		// make this read request transaction id 1, with a 300 millisecond tcp timeout
+		readResult, readErr := modbusclient.TCPRead(conn, 300, 1, modbusclient.FUNCTION_READ_HOLDING_REGISTERS, false, 0x0C, readData, trace)
+		if readErr != nil {
+			log.Println(readErr)
+		}
+		log.Println(readResult)
+	}
 	// attempt to read one (0x01) holding registers starting at address 200
 	readData := make([]byte, 3)
 	readData[0] = byte(200 >> 8)   // (High Byte)
 	readData[1] = byte(200 & 0xff) // (Low Byte)
-	readData[2] = 0x01
+	readData[2] = 0x0C
 
 	// make this read request transaction id 1, with a 300 millisecond tcp timeout
 	readResult, readErr := modbusclient.TCPRead(conn, 300, 1, modbusclient.FUNCTION_READ_HOLDING_REGISTERS, false, 0x0C, readData, trace)
@@ -45,18 +54,6 @@ func main() {
 		log.Println(readErr)
 	}
 	log.Println(readResult)
-
-	// attempt to write to a single coil at address 300
-	writeData := make([]byte, 3)
-	writeData[0] = byte(300 >> 8)   // (High Byte)
-	writeData[1] = byte(300 & 0xff) // (Low Byte)
-	writeData[2] = 0x00             // 0xff turns the coil on; 0x00 turns the coil off
-	// make this read request transaction id 2, with a 300 millisecond tcp timeout
-	writeResult, writeErr := modbusclient.TCPWrite(conn, 300, 2, modbusclient.FUNCTION_WRITE_SINGLE_COIL, false, 0x0C, writeData, trace)
-	if writeErr != nil {
-		log.Println(writeErr)
-	}
-	log.Println(writeResult)
 
 	modbusclient.DisconnectTCP(conn)
 
@@ -66,71 +63,3 @@ func connectModbus() {
 
 }
 
-func getLat() string {
-	out, err := exec.Command("cat", "file.txt").Output()
-	if (err != nil) || (len(out) == 0) {
-		return "NA;NA"
-	}
-
-	strOut := strings.Replace(string(out), "\r\n", "", -1)
-	fltOut, _ := strconv.ParseFloat(strOut, 64)
-
-	if fltOut == 0 {
-		return "NA;NA"
-	}
-	if fltOut > 0 {
-		return fmt.Sprintf("%.0f;N", fltOut*100)
-	}
-	return fmt.Sprintf("%.0f;S", fltOut*-100)
-
-}
-
-func readByLines(networkStatus *string) {
-	fileHanler, err := os.OpenFile("buffer.buf", os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		log.Println("cant read buffer file")
-		return
-	}
-	defer fileHanler.Close()
-
-	fileScanner := bufio.NewScanner(fileHanler)
-	var msg string
-	for fileScanner.Scan() {
-		msg = fileScanner.Text()
-		log.Println(msg)
-		switch *networkStatus {
-		case "postBuffering":
-			log.Printf("send msg>%s", msg)
-		case "buffering":
-			log.Printf("save to new file>%s", msg)
-		default:
-			log.Println("buffer unexpected error, networkStatus is not postBuffering or buffering")
-			return
-		}
-	}
-	if err = fileScanner.Err(); err != nil {
-		log.Fatalf("scan file error: %v", err)
-		return
-	}
-}
-
-func runMaShit() string {
-	strInp := "Hehello"
-	fileHandler, err := os.Create("buffer.buf")
-	if err != nil {
-		log.Printf("open error %v, try to create buffer.buf\n", err)
-		if fileHandler, err = os.Create("buffer.buf"); err != nil {
-			log.Println("create error", err)
-			os.Exit(1)
-		}
-		exec.Command("chmod +x buffer.buf")
-	}
-	defer fileHandler.Close()
-
-	for i := 1; i < 10; i++ {
-		log.Println("write")
-
-		fileHandler.WriteString(strInp + strconv.Itoa(i))
-	}
-	return "done"
-}
