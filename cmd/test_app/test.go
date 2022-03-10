fdda8c42404290186d3ba95f4f82485eab7883e7
Nikolai Kottcov <nikolafgh@gmail.com>
2022-03-10 17:25:35 +0200

    add data converter

--- cmd/test_app/test.go
+++ cmd/test_app/test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"encoding/hex"
 	"github.com/goburrow/modbus"
 	"log"
 	"math"
@@ -14,35 +15,98 @@ func main() {
 	res := getModbusData()
 	log.Println(res)
 	log.Println(res[0:8])
-	log.Println(getSigInt8b(res))
+	log.Println(getSigInt8(res))
 }
 
-func getUnsInt8b(bytes []byte) []uint8 {
-	return bytes
-}
-
-func getSigInt8b(bytes []byte) []int8 {
+func getSigInt8(bytes []byte) []int8 {
 	var resul []int8
 	for _, d := range bytes {
-		log.Println(int8(d))
 		resul = append(resul, int8(d))
 	}
 	return resul
-
 }
 
-func getFloat32bytes3412(bytes []byte, p1 int, p2 int, p3 int, p4 int) []float32 {
-	var resul []float32
-	for i := 3; i <= len(bytes)-4; i += 4 {
-		resul = append(resul, Float32frombytes([]byte{bytes[i-(4-p1)], bytes[i-(4-p2)], bytes[i-(4-p3)], bytes[i-(4-p4)]}))
+func getUnsInt8(bytes []byte) []uint8 {
+	return bytes
+}
+
+func getSigInt16(bytes []byte, p1 int, p2 int) []int16 {
+	var resul []int16
+	for i := 1; i <= len(bytes)-2; i += 2 {
+		resul = append(resul, byteToInt16([]byte{bytes[i-(2-p1)], bytes[i-(2-p2)]}))
 	}
 	return resul
 }
 
-func Float32frombytes(bytes []byte) float32 {
-	bits := binary.BigEndian.Uint32(bytes)
-	float := math.Float32frombits(bits)
-	return float
+func getUnsInt16(bytes []byte, p1 int, p2 int) []uint16 {
+	var resul []uint16
+	for i := 1; i <= len(bytes)-2; i += 2 {
+		resul = append(resul, byteToUint16([]byte{bytes[i-(2-p1)], bytes[i-(2-p2)]}))
+	}
+	return resul
+}
+
+func getSigInt32(bytes []byte, p1 int, p2 int, p3 int, p4 int) []int32 {
+	var resul []int32
+	for i := 1; i <= len(bytes)-2; i += 2 {
+		resul = append(resul, byteToInt32([]byte{bytes[i-(4-p1)], bytes[i-(4-p2)], bytes[i-(4-p3)], bytes[i-(4-p4)]}))
+	}
+	return resul
+}
+
+func getUnsInt32(bytes []byte, p1 int, p2 int, p3 int, p4 int) []uint32 {
+	var resul []uint32
+	for i := 1; i <= len(bytes)-2; i += 2 {
+		resul = append(resul, byteToUint32([]byte{bytes[i-(4-p1)], bytes[i-(4-p2)], bytes[i-(4-p3)], bytes[i-(4-p4)]}))
+	}
+	return resul
+}
+
+func getFloat32(bytes []byte, p1 int, p2 int, p3 int, p4 int) []float32 {
+	var resul []float32
+	for i := 3; i <= len(bytes)-4; i += 4 {
+		resul = append(resul, byteToFloat32([]byte{bytes[i-(4-p1)], bytes[i-(4-p2)], bytes[i-(4-p3)], bytes[i-(4-p4)]}))
+	}
+	return resul
+}
+
+func getAscii(bytes []byte) string {
+	return byteToAscii(bytes)
+}
+
+func getHex(bytes []byte) []string {
+	var resul []string
+	for i := 1; i < len(bytes)-2; i += 2 {
+		resul = append(resul, byteToHex([]byte{bytes[i-1], bytes[i]}))
+	}
+	return resul
+}
+
+func byteToHex(bytes []byte) string {
+	return hex.EncodeToString(bytes)
+}
+
+func byteToAscii(bytes []byte) string {
+	return string(bytes)
+}
+
+func byteToInt16(bytes []byte) int16 {
+	return int16(binary.BigEndian.Uint16(bytes))
+}
+
+func byteToUint16(bytes []byte) uint16 {
+	return binary.BigEndian.Uint16(bytes)
+}
+
+func byteToInt32(bytes []byte) int32 {
+	return int32(binary.BigEndian.Uint32(bytes))
+}
+func byteToUint32(bytes []byte) uint32 {
+	return binary.BigEndian.Uint32(bytes)
+}
+
+func byteToFloat32(bytes []byte) float32 {
+	return math.Float32frombits(binary.BigEndian.Uint32(bytes))
 }
 
 func getModbusData() []byte {
--- cmd/test_app/test2.go
+++ cmd/test_app/test2.go
@@ -3,11 +3,7 @@ package main
 import "log"
 
 func main() {
-	a := []byte{1, 16, 36, 64, 127, 128, 156, 253}
-
-	for _, d := range a {
-		log.Println(d)
-		log.Println(int8(d))
-	}
-
+	a := []byte{65, 66, 67}
+	log.Println(a)
+	log.Println(string(a))
 }
