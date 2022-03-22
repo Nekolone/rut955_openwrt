cac4e7e1cbc62796f84b71b6f99fe176312bf277
Nikolai Kottcov <nikolafgh@gmail.com>
2022-03-22 18:51:17 +0200

    vs code, i cant work on it

--- cmd/test_app/test.go
+++ cmd/test_app/test.go
@@ -2,60 +2,66 @@ package main
 
 import (
 	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
+
+	//import the Paho Go MQTT library
+	"os"
 	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+//define a function for the default message handler
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
-var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-	fmt.Println("Connected")
-}
-
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("Connect lost: %v", err)
+var msgHandl MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("get TOPIC: %s\n", msg.Topic())
+	fmt.Printf("get MSG: %s\n", msg.Payload())
 }
 
 func main() {
-	var broker = "192.168.35.72"
-	var port = 1883
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
-	opts.SetClientID("1")
-	//opts.SetUsername("emqx")
-	//opts.SetPassword("public")
-	opts.SetDefaultPublishHandler(messagePubHandler)
-	opts.OnConnect = connectHandler
-	opts.OnConnectionLost = connectLostHandler
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+	opts := MQTT.NewClientOptions().AddBroker("tcp://192.168.35.72:1883")
+	// opts.SetClientID("go-simple")
+	// opts.SetDefaultPublishHandler(f)
 
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
+	//create and start a client using the above ClientOptions
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	sub(client)
-	//publish(client)
+	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := c.Subscribe("modbusData", 0, msgHandl); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
 
-	time.Sleep(5 * time.Second)
+	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+	//from the server after sending each message
+	// go publishThis(c)
 
-	client.Disconnect(250)
+	log.Println("stopped")
+	time.Sleep(333 * time.Second)
+
+	//unsubscribe from /go-mqtt/sample
+	if token := c.Unsubscribe("#"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
 }
 
-//func publish(client mqtt.Client) {
-//	num := 10
-//	for i := 0; i < num; i++ {
-//		//text := fmt.Sprintf("Message %d", i)
-//		token := client.Publish("testTopic", 0, false, text)
-//		token.Wait()
-//		time.Sleep(time.Second)
-//	}
-//}
-
-func sub(client mqtt.Client) {
-	topic := "#"
-	token := client.Subscribe(topic, 1, nil)
-	token.Wait()
-	fmt.Printf("Subscribed to topic: %s", topic)
+func publishThis(c MQTT.Client) {
+	for i := 0; i < 5; i++ {
+		text := fmt.Sprintf("this is msg #%d!", i)
+		token := c.Publish("go-mqtt/sample", 0, false, text)
+		token.Wait()
+	}
 }
