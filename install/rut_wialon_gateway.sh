8f76f31710a22e3148abc8de89be245c9ab95eca
Nikolai Kottcov <nikolafgh@gmail.com>
2022-04-07 04:22:45 +0300

    installer

--- install/rut_wialon_gateway.sh
+++ install/rut_wialon_gateway.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-./rut_wialon_gateway &
+./rut_wialon_gateway_app &
 
 
 while :
@@ -21,13 +21,13 @@ do
     fi
 
     echo "$UPD_VER" > /overlay/wialon_rut955_gateway/curr_version
-    pgrep "./rut_wialon_gateway" | xargs kill
+    pgrep "./rut_wialon_gateway_app" | xargs kill
 
     if ! wget https://raw.githubusercontent.com/Nekolone/rut955_openwrt/main/main_app ; then
         continue
     fi
 
-    mv main_app rut_wialon_gateway
-    chmod +x rut_wialon_gateway
-    ./rut_wialon_gateway &
+    mv main_app rut_wialon_gateway_app
+    chmod +x rut_wialon_gateway_app
+    ./rut_wialon_gateway_app &
 done
